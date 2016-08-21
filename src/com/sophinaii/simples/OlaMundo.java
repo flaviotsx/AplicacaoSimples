48c99f94e7d7e96bf8ee5c7781bb4a314b3a580e
Flavio Tavares Silva <flavio.tsx@gmail.com>
2016-08-21 12:32:15 -0300

    AplicacaoSimples

---
+++ src/com/sophinaii/simples/OlaMundo.java
@@ -0,0 +1,33 @@
+package com.sophinaii.simples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class OlaMundo
+ */
+@WebServlet("/olamundo")
+public class OlaMundo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Olá Mundo</h1>");
+		out.println("<p>Teste Servlet</p>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
